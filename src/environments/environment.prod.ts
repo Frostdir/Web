560e62e27f02e18712e1319806b1f9cc6738ddda
Frostdir <relaks_maks@mail.ru>
2021-11-18 22:53:05 +0300

    верстка и разбиение на компоненты, вся логика и мне было очень лень разбивать на разные коммиты, простите(

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'AIzaSyDZaxEbPiMTE5pCLY1l1jcwk_AHGtHFCuc',
+  fbDbUrl: 'https://online-smart-shop-9ec3c-default-rtdb.firebaseio.com/'
 };
